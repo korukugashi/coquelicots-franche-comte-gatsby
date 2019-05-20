af35c20efee4b15bd69454311aa65ac677784072
Nicolas Bland <nicolas.bland@protonmail.com>
2019-05-20 16:33:26 +0200

    Supprime le service worker : cause des soucis d'actualisation sur un site one page

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,9 +30,7 @@ module.exports = {
         icon: `src/images/logo-les-coquelicots.svg`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
+    `gatsby-plugin-remove-serviceworker`,
     {
       resolve: 'gatsby-plugin-svgr',
       options: {
